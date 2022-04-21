f86927603793ff526753dc557992d10cce09239b
meraxx27 <60677127+meraxx27@users.noreply.github.com>
2022-04-20 18:21:34 -0700

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/meraxx27/practica.git', 'master') { node ->
+            node / gitConfigName('meraxx27')
+            node / gitConfigEmail('miguelmeraz425@gmail.com')
         }
     }
     triggers {
